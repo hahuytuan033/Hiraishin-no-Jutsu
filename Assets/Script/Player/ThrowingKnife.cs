de0bfc4ce686a72ee3cd6fbc383b460988653088
huytuan033 <hahuytuan033@gmail.com>
2024-10-12 21:27:36 +0700

    The direction of the kunai follows the direction of the mouse

--- Assets/Script/Player/ThrowingKnife.cs
+++ Assets/Script/Player/ThrowingKnife.cs
@@ -7,22 +7,24 @@ public class ThrowingKnife : MonoBehaviour
     public Transform firePoint;
     public GameObject kunaiPrefab;
 
-    public float kunaiForce= 20f;
+    public float kunaiForce = 20f;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             Shoot();
         }
     }
 
     void Shoot()
-    {
+    {   //The direction of the kunai follows the direction of the mouse
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mousePos - firePoint.position).normalized;
 
-        GameObject kunai= Instantiate(kunaiPrefab,firePoint.position, firePoint.rotation);
-        Rigidbody2D rb= kunai.GetComponent<Rigidbody2D>();
-        rb.AddForce(firePoint.up * kunaiForce, ForceMode2D.Impulse);
+        GameObject kunai = Instantiate(kunaiPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = kunai.GetComponent<Rigidbody2D>();
+        rb.AddForce(direction * kunaiForce, ForceMode2D.Impulse);
     }
 }
