ba4fd564e5d0a2880e686e18cd35d498253fff8f
huytuan033 <hahuytuan033@gmail.com>
2024-10-12 16:29:21 +0700

    player movement with input system

---
+++ Assets/Script/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 movementInput;
+    [SerializeField] private float speed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = movementInput * speed;
+    }
+
+    private void OnMove(InputValue inputValue)
+    {
+        movementInput = inputValue.Get<Vector2>();
+    }
+}
