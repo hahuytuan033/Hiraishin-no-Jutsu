b0e2cca29a7531e7f5e4b1e89a1a06fcc4f9f1c1
huytuan033 <hahuytuan033@gmail.com>
2024-10-15 17:00:15 +0700

    Test Movement and animation for Player (false)

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,12 +8,15 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Vector2 movementInput;
+    private Vector2 lastMove;
     [SerializeField] private float speed;
+    private Animator anim;
 
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -21,11 +25,28 @@ public class PlayerMovement : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        rb.velocity = movementInput * speed;   
+        rb.velocity = movementInput * speed;
     }
 
     private void OnMove(InputValue inputValue)
     {
         movementInput = inputValue.Get<Vector2>();
+        if (movementInput != Vector2.zero)
+        {
+            lastMove= movementInput;
+            Animate();
+        }
+        else
+        {
+            anim.SetFloat("LastMoveX", lastMove.x);
+            anim.SetFloat("LastMoveY", lastMove.y);
+        }
+    }
+
+    void Animate()
+    {
+        anim.SetFloat("XInput", movementInput.x);
+        anim.SetFloat("YInput", movementInput.y);
+        anim.SetFloat("Moving", movementInput.magnitude);
     }
 }
